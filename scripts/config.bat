9b432275cea128fc26f5ee3db94942f15212771e
Andrew <gammaclone@gmail.com>
2020-01-18 21:02:30 +0200

    Simple fix

--- scripts/config.bat
+++ scripts/config.bat
@@ -5,7 +5,6 @@ set main_folder=%cd%\src\
 cd res
 cd images
 for /r %%i in (*.qrc) do (
-echo %%i
 if not exist %main_folder%%%~ni_rc.py (
 D:\Python\BeeIsland\venv\Scripts\pyrcc5 %%~nxi -o %main_folder%%%~ni_rc.py
 )
