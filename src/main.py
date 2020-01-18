a40358f6742d779908da3524695c8d260f32f816
Andrew <gammaclone@gmail.com>
2020-01-18 21:03:07 +0200

    Added main form

--- src/main.py
+++ src/main.py
@@ -1,5 +1,25 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+
+Form, Window = uic.loadUiType("main_form.ui")
+
+
+class Application:
+    def __init__(self):
+        self.app = QApplication([])
+        self.window = Window()
+        self.form = Form()
+
+        self.form.setupUi(self.window)
+
+    def start(self):
+        self.window.show()
+        self.app.exec_()
+
+
 def main():
-    print("Hello, world")
+    app = Application()
+    app.start()
 
 
 if __name__ == "__main__":
