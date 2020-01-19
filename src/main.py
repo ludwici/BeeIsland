f6b3279437189fed8e729077ce0e72367db82860
Andrew <gammaclone@gmail.com>
2020-01-19 22:53:22 +0200

    Added player and widgets on code

--- src/main.py
+++ src/main.py
@@ -1,24 +1,37 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QTableWidget, QTextEdit
+
+from src.Player import Player
+from src.Reporter import Reporter
 
 Form, Window = uic.loadUiType("main_form.ui")
 
 
 class Application:
-    def __init__(self):
+    def __init__(self, player: Player):
         self.app = QApplication([])
         self.window = Window()
         self.form = Form()
 
         self.form.setupUi(self.window)
 
+        self.player = player
+
+        self.resources_table = self.window.findChild(QTableWidget, "resourceTable")
+        self.board = self.window.findChild(QTextEdit, "board")
+
+        self.reporter = Reporter(self.board)
+
     def start(self):
+        self.reporter.post("Вы прибыли на ферму")
+
         self.window.show()
         self.app.exec_()
 
 
 def main():
-    app = Application()
+    p = Player()
+    app = Application(p)
     app.start()
 
 
