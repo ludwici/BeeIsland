60bbd4e434acf7f26673b9d7d72e381f815fda14
Andrew <gammaclone@gmail.com>
2020-01-24 17:54:56 +0200

    Added hive restrictions

--- src/Farm.py
+++ src/Farm.py
@@ -1,7 +1,25 @@
+from src.Hive import Hive
+
+
 class Farm:
     def __init__(self):
         self.__hive_list = []
+        self.max_active_hive_count = 3
+        self.max_hive_slots = 6
+        self.max_hive_level = 5
+
+    def size(self):
+        return len(self.hive_list)
 
     @property
     def hive_list(self):
         return self.__hive_list
+
+    def addHive(self, hive: Hive):
+        if self.size() >= self.max_active_hive_count:
+            return
+
+        if hive.current_level > self.max_hive_level:
+            return
+
+        self.hive_list.append(hive)
