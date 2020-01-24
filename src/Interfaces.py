2c10ab81818ca284dd9538af09b154870e0c98cc
Andrew <gammaclone@gmail.com>
2020-01-24 06:41:07 +0200

    Created interface to all classes, which can upgraded

---
+++ src/Interfaces.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+
+
+class Levelable(ABC):
+    def __init__(self):
+        self.__current_level = 0
+        self.__max_level = 10
+        self.xp_enabled = True
+        self.__current_xp = 0
+        self.max_xp = 0
+
+    @property
+    def max_level(self):
+        return self.__max_level
+
+    @property
+    def current_xp(self):
+        return self.current_xp
+
+    @current_xp.setter
+    def current_xp(self, value):
+        if not self.xp_enabled:
+            return
+        if self.current_xp > self.max_xp:
+            over_xp = self.current_xp = self.max_xp
+            self.changeLevelTo(self.current_level+1)
+            self.current_xp += over_xp
+        elif self.current_xp == self.max_xp:
+            self.changeLevelTo(self.current_level+1)
+
+    @property
+    def current_level(self):
+        return self.__current_level
+
+    def changeLevelTo(self, level: int) -> bool:
+        if (self.current_level + level) <= self.max_level:
+            self.__current_level = level
+            self.xp_enabled = False
+            self.current_xp = 0
+            return True
+        return False
