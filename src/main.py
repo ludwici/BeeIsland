7e0f5de4502cef5cdfce59f4b662a7362a1093c2
Andrew <gammaclone@gmail.com>
2020-01-24 11:25:49 +0200

    Added map to form

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QTableWidget, QTextEdit
+from PyQt5.QtGui import QPixmap, QBrush, QColor
+from PyQt5.QtWidgets import QApplication, QTableWidget, QTextEdit, QWidget, QGraphicsView, QDoubleSpinBox
 
+from src.MapView import MapView
 from src.Player import Player
 from src.Reporter import Reporter
 
@@ -11,6 +13,10 @@ class Application:
     def __init__(self, player: Player):
         self.app = QApplication([])
         self.window = Window()
+
+        self.map_view = MapView(self.window)
+        self.map_view.setMaximumWidth(761)
+        self.map_view.setPhoto(QPixmap("../res/images/map1.jpg"))
         self.form = Form()
 
         self.form.setupUi(self.window)
@@ -19,6 +25,12 @@ class Application:
 
         self.resources_table = self.window.findChild(QTableWidget, "resourceTable")
         self.board = self.window.findChild(QTextEdit, "board")
+        self.map_tap = self.window.findChild(QWidget, "MapTab")
+        self.zoom_factor = self.window.findChild(QDoubleSpinBox, "zoomFactor")
+        self.zoom_factor.setValue(self.map_view.zoom)
+        self.map_view.zoom_spinbox = self.zoom_factor
+
+        self.map_tap.layout().addWidget(self.map_view)
 
         self.reporter = Reporter(self.board)
 
