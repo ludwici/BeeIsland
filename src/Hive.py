6209097566c9ff9101ec06700b4e5ee1dc06fd32
Andrew <gammaclone@gmail.com>
2020-01-24 06:42:07 +0200

    Added class for representation hive entity

---
+++ src/Hive.py
@@ -0,0 +1,37 @@
+from src.Bee import Bee
+from src.Interfaces import Levelable
+
+
+class Hive(Levelable):
+    def __init__(self):
+        Levelable.__init__(self)
+        self.max_xp = 5
+        self.max_size = 0
+        self.__bee_list = []
+
+    def changeLevelTo(self, level: int) -> bool:
+        if not super().changeLevelTo(level):
+            return False
+
+        if level == 1:
+            self.max_size = 5
+            self.max_xp = 5
+        elif level == 2:
+            self.max_size += 2
+            self.max_xp = 10
+
+        return True
+
+    @property
+    def size(self):
+        return len(self.__bee_list)
+
+    @property
+    def bee_list(self):
+        return self.__bee_list
+
+    def addBee(self, bee: Bee):
+        if self.size >= self.max_size:
+            return
+
+        self.__bee_list.append(bee)
