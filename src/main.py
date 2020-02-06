05f175b97a305049d4d4dfb3c5eb92aa4b7e4927
Andrew <gammaclone@gmail.com>
2020-02-06 21:18:33 +0200

    Game-loop logic moved to RenderWindow.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,9 @@
-import pygame
-
-from src.Player import Player
 from src.Windows.MapWindow import MapWindow
 
 
 def main():
     main_window = MapWindow()
-    while main_window.loop():
-        pass
-
-    # p = Player()
+    main_window.start()
 
 
 if __name__ == "__main__":
