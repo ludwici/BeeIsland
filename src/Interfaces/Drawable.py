31add20fe26bfc66b31f6759810761f6a681adf0
Andrew <gammaclone@gmail.com>
2020-02-06 21:11:05 +0200

    Now this class knows who its parent is and added method for change position

--- src/Interfaces/Drawable.py
+++ src/Interfaces/Drawable.py
@@ -1,13 +1,18 @@
-from abc import ABC, abstractmethod
-
 import pygame
+from abc import ABC, abstractmethod
+from src.Windows import RenderWindow
 from pygame.rect import Rect
 
 
 class Drawable(ABC):
-    def __init__(self, position=Rect((0, 0, 0, 0))) -> None:
+    def __init__(self, parent: RenderWindow, position=Rect((0, 0, 0, 0))) -> None:
+        self.parent = parent
         self.rect = position
         self.color = (0, 0, 0)
+
+    def setPosition(self, position):
+        self.rect.x = position.x
+        self.rect.y = position.y
 
     @abstractmethod
     def draw(self, screen: pygame.Surface) -> None:
