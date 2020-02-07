d1fac8a0f579a8e2e5e9fe0d879ceca4d4ca0aa6
Andrew <gammaclone@gmail.com>
2020-02-08 01:16:56 +0200

    Added first quests to map Added blank to zooming image Optimize append zones to list

--- src/Windows/MapWindow.py
+++ src/Windows/MapWindow.py
@@ -1,6 +1,7 @@
 import pygame
 
 from src.MapZone import MapZone
+from src.Quests.Match3 import Match3
 from src.Windows.RenderWindow import RenderWindow
 
 
@@ -11,24 +12,29 @@ class MapWindow(RenderWindow):
         self.zones = []
         self.initZones()
 
+    def zooming(self) -> None:
+        self.bg_image = pygame.transform.rotozoom(self.bg_image, 0, 1.1)
+
     def initZones(self) -> None:
-        zone1 = MapZone("Zone1", pos_x=20, pos_y=75)
-        zone2 = MapZone("Zone2", pos_x=83, pos_y=375)
-        zone3 = MapZone("Zone3", pos_x=488, pos_y=183)
-        zone4 = MapZone("Zone4", pos_x=298, pos_y=33)
-        zone5 = MapZone("Zone5", pos_x=415, pos_y=465)
-        zone6 = MapZone("Zone6", pos_x=285, pos_y=214)
-        zone7 = MapZone("Zone7", pos_x=383, pos_y=578, has_fog=False)
+        zone1 = MapZone(self, "Zone1", pos_x=20, pos_y=75)
+        zone2 = MapZone(self, "Zone2", pos_x=83, pos_y=375)
+        zone3 = MapZone(self, "Zone3", pos_x=488, pos_y=183)
+        zone4 = MapZone(self, "Zone4", pos_x=298, pos_y=33)
+        zone5 = MapZone(self, "Zone5", pos_x=415, pos_y=465)
+        zone6 = MapZone(self, "Zone6", pos_x=285, pos_y=214)
+        zone7 = MapZone(self, "Zone7", pos_x=383, pos_y=578, has_fog=False)
+
+        q1 = Match3(zone=zone1, icon_position=(110, 120))
+        q1.condition = True
+        q1.check_allow()
+
+        q2 = Match3(zone=zone1, icon_position=(90, 160))
+        q2.condition = True
+        q2.check_allow()
 
         zone1.unlock()
 
-        self.zones.append(zone1)
-        self.zones.append(zone2)
-        self.zones.append(zone3)
-        self.zones.append(zone4)
-        self.zones.append(zone5)
-        self.zones.append(zone6)
-        self.zones.append(zone7)
+        self.zones.extend([zone1, zone2, zone3, zone4, zone5, zone6, zone7])
 
     def loop(self) -> bool:
         self.clock.tick(60)
