e5e68a849ac2d57c0e43ed528745f0844cae5bd2
Andrew <gammaclone@gmail.com>
2020-02-08 01:10:55 +0200

    Changed position type from Rect() to tuple(int, int), because Rect() stored redundant data

--- src/Interfaces/Drawable.py
+++ src/Interfaces/Drawable.py
@@ -1,18 +1,22 @@
 import pygame
 from abc import ABC, abstractmethod
-from src.Windows import RenderWindow
 from pygame.rect import Rect
 
 
 class Drawable(ABC):
-    def __init__(self, parent: RenderWindow, position=Rect((0, 0, 0, 0))) -> None:
+    def __init__(self, parent, position: (int, int)) -> None:
         self.parent = parent
-        self.rect = position  # TODO: add abstract getter
+        self._rect = Rect((0, 0, 0, 0))
+        self.setPosition(position)
         self.color = (0, 0, 0)
 
-    def setPosition(self, position) -> None:
-        self.rect.x = position.x
-        self.rect.y = position.y
+    @property
+    def position(self) -> (int, int):
+        return self._rect.x, self._rect.y
+
+    def setPosition(self, position: (int, int)) -> None:
+        self._rect.x = position[0]
+        self._rect.y = position[1]
 
     @abstractmethod
     def draw(self, screen: pygame.Surface) -> None:
