ffec9016b161e725c2657d5ee5a1cf6e0439538f
Andrew <gammaclone@gmail.com>
2020-02-08 01:14:59 +0200

    Adapted for new logic in Drawable.py Hide hover image (temporary)

--- src/Windows/Button.py
+++ src/Windows/Button.py
@@ -1,29 +1,30 @@
 import pygame
 from src.Interfaces.Drawable import Drawable
-from pygame.rect import Rect
 
 
 class Button(Drawable):
-    def __init__(self, text: str = "", position=Rect((0, 0, 0, 0))) -> None:
-        Drawable.__init__(self, position)
-        self.bg_image = pygame.image.load("../res/images/exit_btn.png").convert_alpha()
-        self.bg_image_hovered = pygame.image.load("../res/images/exit_btn_hovered.png").convert_alpha()
+    def __init__(self, parent, path_to_image: str, text: str = "", position: (int, int) = (0, 0)) -> None:
+        Drawable.__init__(self, parent=parent, position=position)
+        self.image = None
+        self.setImage(path_to_image)
+        # self.bg_image_hovered = pygame.image.load("").convert_alpha()
 
-        self.image = self.bg_image
-
-        self.rect.width = self.image.get_rect().width
-        self.rect.height = self.image.get_rect().height
         self.text = text
         self.hovered = False
 
     def draw(self, screen: pygame.Surface) -> None:
-        screen.blit(self.image, self.rect)
+        screen.blit(self.image, self._rect)
+
+    def setImage(self, path: str) -> None:
+        self.image = pygame.image.load(path).convert_alpha()
+        self._rect.width = self.image.get_rect().width
+        self._rect.height = self.image.get_rect().height
 
     def handle_event(self, event) -> None:
         if event.type == pygame.MOUSEMOTION:
-            self.hovered = self.rect.collidepoint(event.pos)
+            self.hovered = self._rect.collidepoint(event.pos)
 
-        if self.hovered:
-            self.image = self.bg_image_hovered
-        else:
-            self.image = self.bg_image
+        # if self.hovered:
+        #     self.image = self.bg_image_hovered
+        # else:
+        #     self.image = self.bg_image
