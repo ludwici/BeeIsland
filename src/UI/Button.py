6ffd2e4d83ab1b58f8b7eb73d2d88b55f5691ee0
Andrew <gammaclone@gmail.com>
2020-02-14 06:18:50 +0200

    Added logic for clicks Added list of actions, which will be do after click

--- src/UI/Button.py
+++ src/UI/Button.py
@@ -1,4 +1,5 @@
 import pygame
+from typing import Callable
 from src.Interfaces.Drawable import Drawable
 
 
@@ -7,10 +8,15 @@ class Button(Drawable):
         Drawable.__init__(self, parent=parent, position=position)
         self.image = None
         self.setImage(path_to_image)
-        # self.bg_image_hovered = pygame.image.load("").convert_alpha()
-
+        self.action_list = []
         self.text = text
         self.hovered = False
+
+    def update(self, dt) -> None:
+        pass
+
+    def addAction(self, action: Callable) -> None:
+        self.action_list.append(action)
 
     def draw(self, screen: pygame.Surface) -> None:
         screen.blit(self.image, self._rect)
@@ -20,9 +26,16 @@ class Button(Drawable):
         self._rect.width = self.image.get_rect().width
         self._rect.height = self.image.get_rect().height
 
+    def onClick(self) -> None:
+        [a() for a in self.action_list]
+
     def handle_event(self, event) -> None:
         if event.type == pygame.MOUSEMOTION:
             self.hovered = self._rect.collidepoint(event.pos)
+
+        if event.type == pygame.MOUSEBUTTONDOWN and self.hovered:
+            if event.button == 1:
+                self.onClick()
 
         # if self.hovered:
         #     self.image = self.bg_image_hovered
