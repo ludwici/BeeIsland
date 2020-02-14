bf0e1f5bda9cfb27bfab9b9821f6f34267f4bd5a
Andrew <gammaclone@gmail.com>
2020-02-14 06:15:37 +0200

    Added type hints Click on quest icon started new scene

--- src/Quests/Match3.py
+++ src/Quests/Match3.py
@@ -1,6 +1,8 @@
+from src import MapZone
 from src.Interfaces.Questable import Questable
 
 
 class Match3(Questable):
-    def __init__(self, zone, icon_position: (int, int)) -> None:
+    def __init__(self, zone: MapZone, icon_position: (int, int)) -> None:
         Questable.__init__(self, zone=zone, icon_position=icon_position)
+        self.icon_btn.addAction(lambda: self.zone.parent.main_window.changeScene("Match3"))
