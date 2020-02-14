5dc856bfafd3a920e658243992ab945d17a931bf
Andrew <gammaclone@gmail.com>
2020-02-14 06:21:03 +0200

    Added map with all game scenes Added logic for scene change

--- src/RenderWindow.py
+++ src/RenderWindow.py
@@ -2,6 +2,7 @@ import os
 import pygame
 
 from src.Scenes.MapScene import MapScene
+from src.Scenes.Match3Scene import Match3Scene
 from pygame.time import Clock
 
 
@@ -13,9 +14,20 @@ class RenderWindow:
         self.__FPS = 60
         self.size = self.width, self.height = width, height
         self.screen = pygame.display.set_mode(self.size)
+
+        self.scene_map = {
+            "Map": MapScene(self),
+            "Match3": Match3Scene(self)
+        }
+
         self.current_scene = MapScene(self)
+        self.prev_scene = None
         self.done = False
         self.clock = Clock()
+
+    def changeScene(self, scene_name: str) -> None:
+        self.prev_scene = self.current_scene
+        self.current_scene = self.scene_map[scene_name]
 
     def start(self) -> None:
         while not self.done:
@@ -35,4 +47,3 @@ class RenderWindow:
         self.current_scene.update(dt)
         self.current_scene.draw(surface=self.screen)
         pygame.display.flip()
-
