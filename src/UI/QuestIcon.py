632c75a68357132a64fd61f056967697afa727e1
Andrew <gammaclone@gmail.com>
2020-02-14 06:19:56 +0200

    Added update-method

--- src/UI/QuestIcon.py
+++ src/UI/QuestIcon.py
@@ -4,3 +4,6 @@ from src.UI.Button import Button
 class QuestIcon(Button):
     def __init__(self, parent, path_to_image: str, position: (int, int)) -> None:
         Button.__init__(self, parent=parent, path_to_image=path_to_image, position=position)
+
+    def update(self, dt) -> None:
+        pass
