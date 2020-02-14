068ee1ea0d3a9b0766f9c727e577fd8fe1e9d9fd
Andrew <gammaclone@gmail.com>
2020-02-14 18:42:11 +0200

    Added type hints

--- src/Scenes/Match3Scene.py
+++ src/Scenes/Match3Scene.py
@@ -1,4 +1,6 @@
 import pygame
+
+from pygame.event import Event
 from src.Scenes.Scene import Scene
 
 
@@ -7,11 +9,14 @@ class Match3Scene(Scene):
         Scene.__init__(self, main_window=main_window)
         self.bg_image = pygame.image.load("../res/images/quest_bg1.png").convert_alpha()
 
-    def update(self, dt) -> None:
+    def update(self, dt: float) -> None:
         pass
 
-    def handle_events(self, event) -> None:
+    def handle_events(self, event: Event) -> None:
         pass
 
-    def draw(self, surface) -> None:
+    def on_scene_change(self) -> None:
+        super().on_scene_change()
+
+    def draw(self, surface: pygame.Surface) -> None:
         surface.blit(self.bg_image, self.bg_image.get_rect())
