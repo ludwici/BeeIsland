39c425cdc59d23aded5004ce8d37a40bb9803f9d
Andrew <gammaclone@gmail.com>
2020-02-21 04:58:27 +0200

    Added tests for BeeNest Fixed tests for min-max values

--- test/TestEntities.py
+++ test/TestEntities.py
@@ -1,32 +1,51 @@
 import sys
 import unittest
 sys.path.append('../')
+from src.BeeNest import BeeNest
 from src.Bee import Bee
 
 
 class TestEntities(unittest.TestCase):
-    def test_xp_1(self):
+    def test_bee_xp_1(self):
         b = Bee(1)
         xp = 160
         b.give_xp(xp)
-        self.assertEqual(b.current_xp, 10)
+        self.assertEqual(b.current_xp, 60)
 
-    def test_xp_2(self):
+    def test_bee_xp_2(self):
         b = Bee(1)
         b.give_xp(70)
         self.assertEqual(b.current_level, 1)
         b.give_xp(100)
         self.assertEqual(b.current_level, 2)
-        self.check_xp(b)
+        self.test_bee_min_max(b)
 
     def test_bee(self):
         b = Bee(1)
         b.give_xp(150)
 
-    def check_xp(self, b):
-        self.assertGreater(b.current_xp, 0)
-        self.assertGreater(b.current_level, 0)
-        self.assertGreater(b.current_hp, 0)
+    def test_add_bee_to_nest(self):
+        b = Bee(1)
+        n = BeeNest(1)
+
+        n.add_bee(b)
+        n.add_bee(b)
+        n.add_bee(b)
+        n.add_bee(b)
+        self.test_nest_min_max(n)
+
+    def test_nest_min_max(self, n=BeeNest()):
+        self.assertGreaterEqual(n.current_xp, 0)
+        self.assertGreaterEqual(n.current_level, 1)
+
+        self.assertLessEqual(n.current_xp, n.max_xp)
+        self.assertLessEqual(n.current_level, n.max_level)
+        self.assertLessEqual(n.size, n.max_size)
+
+    def test_bee_min_max(self, b=Bee()):
+        self.assertGreaterEqual(b.current_xp, 0)
+        self.assertGreaterEqual(b.current_level, 1)
+        self.assertGreaterEqual(b.current_hp, 0)
 
         self.assertLessEqual(b.current_xp, b.max_xp)
         self.assertLessEqual(b.current_level, b.max_level)
