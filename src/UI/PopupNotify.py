6b23fada59c032ebf9d6fef80cb1ddaa40004de4
Andrew <gammaclone@gmail.com>
2020-02-27 10:47:02 +0200

    Added method for set up background for popup If time to kill is equal 0, then popup not destroyed Adapted class to create children

--- src/UI/PopupNotify.py
+++ src/UI/PopupNotify.py
@@ -6,17 +6,17 @@ from src.Scenes import Scene
 
 
 class PopupNotify(Drawable):
-    def __init__(self, parent: Scene, position: (int, int) = (0, 0), time_to_kill: int = 3) -> None:
+    def __init__(self, parent: Scene, position: (int, int) = (0, 0), time_to_kill: int = 3, text: str = "") -> None:
         Drawable.__init__(self, parent=parent, position=position)
-        self.bg_image = pygame.image.load("../res/images/popup1.png").convert_alpha()
-        self._rect.width = self.bg_image.get_rect().width
-        self._rect.height = self.bg_image.get_rect().height
+        self._bg_image = None
+        self.set_background("../res/images/popup1.png")
         self.__font = pygame.font.Font("../res/fonts/18480.ttf", 16)
-        self.__time_to_kill = time_to_kill
+        self._time_to_kill = time_to_kill
         self.__start_time = pygame.time.get_ticks()
-        self.text = ""
+        self.text = text
         self.text_image = None
         self.text_rect = None
+        self.set_text(text=text)
 
         # self.close_btn = Button()
         # self.close_btn.rect.x = self.rect.topright[0] - 28
@@ -25,16 +25,20 @@ class PopupNotify(Drawable):
     def __del__(self):
         print("Destroy Popup")
 
-    @staticmethod
-    def create(scene: Scene, position: (int, int), text: str) -> "PopupNotify":
-        p = PopupNotify(parent=scene, position=position)
-        if text:
-            p.set_text(text)
+    def set_background(self, path_to_image: str) -> None:
+        self._bg_image = pygame.image.load(path_to_image).convert_alpha()
+        self._rect.width = self._bg_image.get_rect().width
+        self._rect.height = self._bg_image.get_rect().height
+
+    @classmethod
+    def create(cls, scene: Scene, position: (int, int), text: str) -> "PopupNotify":
+        p = cls(parent=scene, position=position, text=text)
         p.show()
         return p
 
     def set_text(self, text: str) -> None:
-        self.text = text
+        if not text:
+            return
         self.text_image = self.__font.render(self.text, True, (164, 107, 60))
         self.text_rect = self.text_image.get_rect()
         self.text_rect.x = self.position[0] + 10
@@ -51,11 +55,13 @@ class PopupNotify(Drawable):
         # self.close_btn.handle_event(event)
 
     def update(self, dt: float) -> None:
-        mills = (pygame.time.get_ticks() - self.__start_time) / 1000
-        if mills >= self.__time_to_kill:
-            self.destroy()
+        if self._time_to_kill != 0:
+            mills = (pygame.time.get_ticks() - self.__start_time) / 1000
+            if mills >= self._time_to_kill:
+                self.destroy()
 
     def draw(self, screen: pygame.Surface) -> None:
-        screen.blit(self.bg_image, self._rect)
+        screen.blit(self._bg_image, self._rect)
         # self.close_btn.draw(screen)
-        screen.blit(self.text_image, self.text_rect)
+        if self.text:
+            screen.blit(self.text_image, self.text_rect)
