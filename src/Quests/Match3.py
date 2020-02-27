8313eaa88ce70d5db2ae1b3c1e6f9b8d282e1e39
Andrew <gammaclone@gmail.com>
2020-02-27 10:37:01 +0200

    Removed zone from ctor and added difficult as argument Onclick action moved to handle_event-method

--- src/Quests/Match3.py
+++ src/Quests/Match3.py
@@ -1,8 +1,6 @@
-from src import MapZone
-from src.Interfaces.Questable import Questable
+from src.Interfaces.Questable import Questable, QuestDifficult
 
 
 class Match3(Questable):
-    def __init__(self, zone: MapZone, icon_position: (int, int)) -> None:
-        Questable.__init__(self, zone=zone, icon_position=icon_position)
-        self.icon_btn.add_action(lambda: self.zone.parent.main_window.change_scene("Match3"))
+    def __init__(self, icon_position: (int, int), difficult: QuestDifficult) -> None:
+        Questable.__init__(self, icon_position=icon_position, difficult=difficult)
