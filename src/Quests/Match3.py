19557c35a4e237e9136b872950094a880db05d0f
Andrew <gammaclone@gmail.com>
2020-03-29 19:21:19 +0300

    Added quest title

--- src/Quests/Match3.py
+++ src/Quests/Match3.py
@@ -2,5 +2,5 @@ from src.Interfaces.Questable import Questable, QuestDifficult
 
 
 class Match3(Questable):
-    def __init__(self, icon_position: (int, int), difficult: QuestDifficult) -> None:
-        Questable.__init__(self, icon_position=icon_position, difficult=difficult)
+    def __init__(self, icon_position: (int, int), difficult: QuestDifficult, quest_title: str = "Три в ряд") -> None:
+        Questable.__init__(self, icon_position=icon_position, difficult=difficult, quest_title=quest_title)
