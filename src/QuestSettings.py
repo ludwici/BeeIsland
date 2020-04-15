b80204598b78bb3159ab86986241bcaf4aa03b09
Andrew <gammaclone@gmail.com>
2020-04-15 06:11:52 +0300

    New class, which pass data to quest

---
+++ src/QuestSettings.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+from src.ResourceBag import ResourceBag
+
+
+class QuestDifficult(Enum):
+    EASY = 1
+    MEDIUM = 2
+    HARD = 3
+
+
+class QuestSettings:
+    def __init__(self) -> None:
+        self.difficult = QuestDifficult.EASY
+        self.reward_res = ResourceBag()
+
+    def set_difficult(self, difficult: QuestDifficult) -> None:
+        self.difficult = difficult
