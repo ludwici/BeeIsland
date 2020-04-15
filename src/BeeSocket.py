46fed5669f6ceee2c08d888a82f993249610b89c
Andrew <gammaclone@gmail.com>
2020-04-15 11:38:58 +0300

    Added status

--- src/BeeSocket.py
+++ src/BeeSocket.py
@@ -2,8 +2,9 @@ from src.UI.Button import Button
 
 
 class BeeSocket(Button):
-    def __init__(self, parent, path_to_image: str, position: (int, int)) -> None:
+    def __init__(self, parent, path_to_image: str, position: (int, int), is_active=True) -> None:
         Button.__init__(self, parent=parent, path_to_image=path_to_image, position=position)
+        self.is_active = is_active
         self.bee = None
 
     def on_click(self) -> None:
