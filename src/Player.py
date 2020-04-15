99bbe08fe9d7b16fbd2590833fca3c6dad970954
Andrew <gammaclone@gmail.com>
2020-04-15 11:41:00 +0300

    Added dependencies

--- src/Player.py
+++ src/Player.py
@@ -1,3 +1,4 @@
+from src.Farm import Farm
 from src.ResourceBag import ResourceBag
 
 
@@ -5,3 +6,9 @@ class Player:
     def __init__(self) -> None:
         self.name = "Player"
         self.resources = ResourceBag()
+        self.farm = Farm()
+
+    @property
+    def can_buy_new_hive(self) -> bool:
+        return len(self.farm.hive_list) < self.farm.max_active_hive_count
+
