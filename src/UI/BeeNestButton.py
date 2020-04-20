c93781d8bcd2d7a8379a1dc322eebc0a1b35ae02
Andrew <gammaclone@gmail.com>
2020-04-20 06:19:49 +0300

    Bee nest now - it's radio button

--- src/UI/BeeNestButton.py
+++ src/UI/BeeNestButton.py
@@ -4,18 +4,19 @@ from src.BeeNest import BeeNest
 from src.BeeSocket import BeeSocket
 from src.UI.Button import Button
 from src.UI.PopupNotify import PopupNotify
+from src.UI.RadioGroup import RadioGroup
 
 
 class BeeNestButton(Button):
-    def __init__(self, parent, path_to_image: str, hovered_image: str = "", position: (int, int) = (0, 0)):
+    def __init__(self, parent, path_to_image: str, hovered_image: str = "", position: (int, int) = (0, 0)) -> None:
         Button.__init__(self, parent=parent, path_to_image=path_to_image, hovered_image=hovered_image,
                         position=position)
         self.hive = None
         self.add_action(self.buy_hive)
-        self.nests = []
+        self.nest_group = RadioGroup()
 
-    def __init_hives(self):
-        positions = []
+    def __init_hives(self) -> None:
+        positions = list()
         positions.append((-33, -41))
         positions.append((positions[0][0] + 36 + 48, positions[0][1]))
         positions.append((-9 - 48, positions[0][1] + 18 + 42))
@@ -23,30 +24,28 @@ class BeeNestButton(Button):
         positions.append((-31, 40 + 42))
         positions.append((positions[0][0] + 36 + 48, 40 + 42))
         for i in range(6):
-            if i >= self.hive.max_size:
-                path = "../res/images/buttons/socket3_normal.png"
-                status = False
-            else:
-                path = "../res/images/buttons/socket1_normal.png"
-                status = True
-            s = BeeSocket(parent=self, path_to_image=path, is_active=status,
-                          position=((self.position[0] + positions[i][0]), self.position[1] + positions[i][1]))
-            self.nests.append(s)
+            lock = i >= self.hive.max_size
+            BeeSocket(parent=self, path_to_image="../res/images/buttons/socket1_normal.png",
+                      selected_image="../res/images/buttons/socket5_normal.png", group=self.nest_group, is_locked=lock,
+                      position=((self.position[0] + positions[i][0]), self.position[1] + positions[i][1]))
+        BeeSocket(parent=self, path_to_image="../res/images/buttons/socket4_normal.png",
+                  selected_image="../res/images/buttons/socket5_normal.png", group=self.nest_group,
+                  position=((self.position[0] + positions[2][0] + 18 + 48), self.position[1] + positions[2][1]))
 
-    def show_honeycombs(self):
+    def show_honeycombs(self) -> None:
         print("{0}/{1} пчёл".format(self.hive.size, self.hive.max_size))
 
     def draw(self, screen: pygame.Surface) -> None:
         super().draw(screen)
         if self.hive:
-            [s.draw(screen) for s in self.nests]
+            self.nest_group.draw(screen)
 
     def handle_event(self, event) -> None:
         super().handle_event(event)
         if self.hive:
-            [s.handle_event(event) for s in self.nests]
+            self.nest_group.handle_event(event)
 
-    def buy_hive(self):
+    def buy_hive(self) -> None:
         if self.parent.player.can_buy_new_hive:
             self.hive = BeeNest()
             self.parent.player.farm.add_hive(self.hive)
@@ -56,4 +55,4 @@ class BeeNestButton(Button):
             self.action_list.clear()
             self.add_action(self.show_honeycombs)
         else:
-            PopupNotify.create(scene=self.parent, text="Вы не можете купить это гнездо")
+            PopupNotify.create(scene=self.parent, text="Вы пока не можете купить это гнездо")
