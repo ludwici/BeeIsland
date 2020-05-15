60c57d6d87c538bd561559af96b853448d515565
Andrew <gammaclone@gmail.com>
2020-05-15 14:42:19 +0300

    Changed hover image on difficulties buttons Setting position to many elements moved to ctor now

--- src/UI/QuestPopup.py
+++ src/UI/QuestPopup.py
@@ -29,14 +29,19 @@ class QuestPopup(PopupNotify):
         position = (Constants.WINDOW_W / 2 - self.bg_rect.width / 2, 70)
         self.set_position(position)
         self.quest_settings = QuestSettings()
+        self.panel_rect = pygame.Rect((self.position[0] + 15, self.position[1], self.get_size()[0] - 15 * 2, 277))
+
         self.quest_label = TextLabel(parent=self, text="Title", position=self.position, font_name="segoeprint",
                                      font_size=16, bold=True, color=(159, 80, 17))
         self.description_label = MultilineTextLabel(parent=self, text="description", position=position,
                                                     font_name="segoeprint", font_size=14, color=(159, 80, 17),
                                                     line_length=self.bg_rect.width - 35 * 2)
-        self.panel_rect = pygame.Rect((self.position[0] + 15, self.position[1], self.get_size()[0] - 15 * 2, 277))
+        self.description_label.set_position((self.position[0] + 35, self.position[1] + 74))
+        self.panel_rect.y = self.description_label.position[1] + self.description_label.get_size()[1] + 45
         self.difficult_label = TextLabel(parent=self, text="Уровень сложности", position=position,
                                          font_name="segoeprint", font_size=14, color=(159, 80, 17))
+        self.difficult_label.set_position(
+            (self.position[0] + self.bg_rect.centerx - self.difficult_label.get_size()[0] / 2, self.panel_rect.y + 8))
         self.bonus_label = TextLabel(parent=self, text="Бонусы", position=position, font_name="segoeprint",
                                      font_size=14, color=(159, 80, 17))
 
@@ -44,21 +49,37 @@ class QuestPopup(PopupNotify):
                                color=(159, 80, 17))
         self.easy_button = TextButton(parent=self, normal_image_path="../res/images/buttons/difficult/easy_normal.png",
                                       text_label=easy_label, text_padding=(19, 0))
-        self.easy_button.set_image_by_state(ButtonState.HOVERED, "../res/images/buttons/difficult/easy_hover.png")
+        self.easy_button.set_position(
+            (self.position[0] + 35, self.difficult_label.position[1] + self.difficult_label.get_size()[1] + 9)
+        )
+        self.easy_button.set_image_by_state(ButtonState.HOVERED, "../res/images/buttons/difficult/easy_hovered.png")
+
         medium_label = TextLabel(parent=self, text="Средний", position=(0, 0), font_name="segoeprint", font_size=14,
                                  color=(138, 36, 12))
         self.medium_button = TextButton(parent=self,
                                         normal_image_path="../res/images/buttons/difficult/medium_normal.png",
                                         text_label=medium_label, text_padding=(19, 0))
-        self.medium_button.set_image_by_state(ButtonState.HOVERED, "../res/images/buttons/difficult/medium_hover.png")
+        self.medium_button.set_position(
+            (self.easy_button.position[0], self.easy_button.position[1] + self.easy_button.get_size()[1])
+        )
+        self.medium_button.set_image_by_state(ButtonState.HOVERED,
+                                              "../res/images/buttons/difficult/medium_hovered.png")
+
         hard_label = TextLabel(parent=self, text="Сложный", position=(0, 0), font_name="segoeprint", font_size=14,
                                color=(159, 17, 17))
         self.hard_button = TextButton(parent=self, normal_image_path="../res/images/buttons/difficult/hard_normal.png",
                                       text_label=hard_label, text_padding=(19, 0))
-        self.hard_button.set_image_by_state(ButtonState.HOVERED, "../res/images/buttons/difficult/hard_hover.png")
+        self.hard_button.set_position(
+            (self.medium_button.position[0], self.medium_button.position[1] + self.medium_button.get_size()[1])
+        )
+        self.hard_button.set_image_by_state(ButtonState.HOVERED, "../res/images/buttons/difficult/hard_hovered.png")
+
         self.rewards_panel = pygame.image.load("../res/images/buttons/difficult/rewards.png").convert_alpha()
         self.bonus_panel = pygame.image.load("../res/images/bonus_list_bg.png")
         self.rewards_rect = self.rewards_panel.get_rect()
+        self.rewards_rect.x = self.easy_button.position[0] + self.easy_button.get_size()[0]
+        self.rewards_rect.y = self.easy_button.position[1]
+
         self.bonus_rect = self.bonus_panel.get_rect()
 
         self.bee_socket_group = RadioGroup()
@@ -72,12 +93,35 @@ class QuestPopup(PopupNotify):
         self.bee_socket_hard.set_image_by_state(ButtonState.SELECTED, "../res/images/buttons/socket5_normal.png")
         self.bee_socket_hard.set_image_by_state(ButtonState.LOCKED, "../res/images/buttons/socket3_normal.png")
 
+        bs_start_y = self.rewards_rect.y
+        for bs in self.bee_socket_group.buttons[:-1]:
+            bs.set_position(
+                (self.rewards_rect.x + self.rewards_rect.width + 46, bs_start_y)
+            )
+            bs_start_y += bs.get_size()[1] + 8
+
+        self.bee_socket_hard.set_position(
+            (self.rewards_rect.x + self.rewards_rect.width + 46, bs_start_y + 15)
+        )
+
         start_label = TextLabel(parent=self, text="Начать", position=(0, 0), font_name="segoeprint", font_size=24,
                                 color=(159, 80, 17))
         self.start_button = TextButton(parent=self, normal_image_path="../res/images/buttons/start_quest_btn.png",
                                        text_label=start_label, text_padding=(44, 0))
+        self.start_button.set_position(
+            (self.position[0] + self.bg_rect.centerx - self.start_button.get_size()[0] / 2,
+             self.panel_rect.y + self.panel_rect.height + 40)
+        )
 
         self.rewards_labels = []
+
+        self.bonus_label.set_position(
+            (self.position[0] + self.bg_rect.centerx - self.bonus_label.get_size()[0] / 2,
+             self.rewards_rect.y + self.rewards_rect.height + 8)
+        )
+
+        self.bonus_rect.x = self.position[0] + 35
+        self.bonus_rect.y = self.bonus_label.position[1] + self.bonus_label.get_size()[1] + 9
 
         self.easy_button.add_action(
             {ButtonEventType.ON_CLICK_LB: lambda d=QuestDifficult.EASY: self.change_difficult(d)}
@@ -99,54 +143,13 @@ class QuestPopup(PopupNotify):
             q.show()
         else:
             q = scene.find_drawable_by_type(QuestPopup)
-            q.set_position((Constants.WINDOW_W / 2 - q.bg_rect.width / 2, 70))
         q.quest = kwargs["quest"]
         q.quest_label.set_text(q.quest.title)
         q.quest_label.set_position(
             (q.position[0] + q.bg_rect.centerx - q.quest_label.get_size()[0] / 2, q.position[1] + 3)
         )
-        q.description_label.set_position((q.position[0] + 35, q.position[1] + 74))
         q.description_label.set_text(q.quest.description)
-        q.panel_rect.y = q.description_label.position[1] + q.description_label.get_size()[1] + 45
-        q.difficult_label.set_position(
-            (q.position[0] + q.bg_rect.centerx - q.difficult_label.get_size()[0] / 2, q.panel_rect.y + 8)
-        )
-        q.easy_button.set_position(
-            (q.position[0] + 35, q.difficult_label.position[1] + q.difficult_label.get_size()[1] + 9)
-        )
-        q.medium_button.set_position(
-            (q.easy_button.position[0], q.easy_button.position[1] + q.easy_button.get_size()[1])
-        )
-        q.hard_button.set_position(
-            (q.medium_button.position[0], q.medium_button.position[1] + q.medium_button.get_size()[1])
-        )
-        q.start_button.set_position(
-            (q.position[0] + q.bg_rect.centerx - q.start_button.get_size()[0] / 2,
-             q.panel_rect.y + q.panel_rect.height + 40)
-        )
-        q.rewards_rect.x = q.easy_button.position[0] + q.easy_button.get_size()[0]
-        q.rewards_rect.y = q.easy_button.position[1]
-
-        bs_start_y = q.rewards_rect.y
-        for bs in q.bee_socket_group.buttons[:-1]:
-            bs.set_position(
-                (q.rewards_rect.x + q.rewards_rect.width + 46, bs_start_y)
-            )
-            bs_start_y += bs.get_size()[1] + 8
-
-        q.bee_socket_hard.set_position(
-            (q.rewards_rect.x + q.rewards_rect.width + 46, bs_start_y + 15)
-        )
-
         q.generate_reward_labels((q.rewards_rect.x + 33, q.rewards_rect.y))
-
-        q.bonus_label.set_position(
-            (q.position[0] + q.bg_rect.centerx - q.bonus_label.get_size()[0] / 2,
-             q.rewards_rect.y + q.rewards_rect.height + 8)
-        )
-
-        q.bonus_rect.x = q.position[0] + 35
-        q.bonus_rect.y = q.bonus_label.position[1] + q.bonus_label.get_size()[1] + 9
 
         q.change_difficult(QuestDifficult.EASY)
         return q
