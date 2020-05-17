81cd203b5577e07ea955683a075c55120e25d692
Andrew <gammaclone@gmail.com>
2020-05-17 21:31:11 +0300

    Added list of bee, which not contained in hives

--- src/Farm.py
+++ src/Farm.py
@@ -1,11 +1,13 @@
 from copy import copy
 
+from src.BeeFamily.Bee import Bee
 from src.BeeNest import BeeNest
 
 
 class Farm:
     def __init__(self) -> None:
         self.__hive_list = []
+        self.__out_of_hive_bee_list = []
         self.max_active_hive_count = 1
         self.max_hive_slots = 6
         self.max_hive_level = 5
@@ -17,6 +19,13 @@ class Farm:
     @property
     def hive_list(self) -> list:
         return copy(self.__hive_list)
+
+    @property
+    def out_of_hive_bee_list(self) -> list:
+        return copy(self.__out_of_hive_bee_list)
+
+    def add_out_of_hive_bee(self, b: Bee) -> None:
+        self.__out_of_hive_bee_list.append(b)
 
     def add_hive(self, hive: BeeNest) -> bool:
         if self.active_hives_count > self.max_active_hive_count:
