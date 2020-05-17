62344a88b38478643a3108b3383b730199cc779a
Andrew <gammaclone@gmail.com>
2020-05-17 21:21:57 +0300

    Moved call ctor of base class to top

--- src/UI/TextButton.py
+++ src/UI/TextButton.py
@@ -7,10 +7,9 @@ from src.UI.Button import Button
 class TextButton(Button):
     def __init__(self, parent, normal_image_path, position: (int, int) = (0, 0),
                  text_label: TextLabel = None, text_padding: (int, int) = (0, 0)) -> None:
+        Button.__init__(self, parent=parent, normal_image_path=normal_image_path, position=position)
         self.text_label = text_label
         self.padding = text_padding
-        Button.__init__(self, parent=parent, normal_image_path=normal_image_path, position=position)
-        # self.set_position(position)
 
     def set_position(self, position: (int, int)) -> None:
         super().set_position(position)
