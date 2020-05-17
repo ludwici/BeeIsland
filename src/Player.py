8bbfe96f6820fa44dd707ca3dcaa1c43a714a10b
Andrew <gammaclone@gmail.com>
2020-05-17 21:31:38 +0300

    Added list of bee, which not contained in hives

--- src/Player.py
+++ src/Player.py
@@ -1,3 +1,4 @@
+from src.BeeFamily.BeeWorker import BeeWorker
 from src.Farm import Farm
 from src.ResourceBag import ResourceBag
 
@@ -7,8 +8,10 @@ class Player:
         self.name = "Player"
         self.resources = ResourceBag()
         self.farm = Farm()
+        self.farm.add_out_of_hive_bee(BeeWorker(parent=self, position=(0, 0)))
+        self.farm.add_out_of_hive_bee(BeeWorker(parent=self, position=(0, 0)))
+        self.farm.add_out_of_hive_bee(BeeWorker(parent=self, position=(0, 0)))
 
     @property
     def can_buy_new_hive(self) -> bool:
         return len(self.farm.hive_list) < self.farm.max_active_hive_count
-
