918fa71abbd5866e3bfb8231a42d4837563c2a96
Andrew <gammaclone@gmail.com>
2020-05-17 21:18:52 +0300

    Create on-click method with new decorator

--- src/UI/QuestIcon.py
+++ src/UI/QuestIcon.py
@@ -1,10 +1,12 @@
-from src.UI.Button import Button, ButtonState
+from src.UI.Button import Button, ButtonState, ButtonEventType
 
 
 class QuestIcon(Button):
     def __init__(self, parent, normal_image_path: str, position: (int, int) = (0, 0),
                  state: ButtonState = ButtonState.NORMAL) -> None:
         Button.__init__(self, parent=parent, normal_image_path=normal_image_path, position=position, state=state)
+        self.show_popup()
 
-    def on_click(self) -> None:
+    @Button.register_event(ButtonEventType.ON_CLICK_LB)
+    def show_popup(self):
         self.parent.show_popup()
