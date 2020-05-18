c0c681cf5ec230c38505005f758074beab4d68a2
Andrew <gammaclone@gmail.com>
2020-05-18 21:29:26 +0300

    ListView do not redraw all elements after every added new item assign operator to item_padding dynamically redraw all elements

--- src/UI/ListView.py
+++ src/UI/ListView.py
@@ -11,11 +11,20 @@ class ListView(Drawable):
                  item_padding: (int, int) = (15, 15)) -> None:
         Drawable.__init__(self, parent=parent, position=position)
         self._data = []
-        self.item_padding = item_padding
+        self._item_padding = item_padding
         self.padding = padding
         self.bg_image = None
         self._items_pos = self.position + self.padding
         self.set_position(position)
+
+    @property
+    def item_padding(self) -> (int, int):
+        return copy(self._item_padding)
+
+    @item_padding.setter
+    def item_padding(self, value: (int, int)):
+        self._item_padding = value
+        self._redraw_list()
 
     def get_data(self) -> list:
         return copy(self._data)
@@ -28,21 +37,27 @@ class ListView(Drawable):
     def set_position(self, position: (int, int)) -> None:
         super().set_position(position)
         self._items_pos = self.position[0] + self.padding[0], self.position[1] + self.padding[1]
+        self._redraw_list()
 
     def _redraw_list(self) -> None:
-        for i in self._data:
-            value = i.get_size()[0] + self._items_pos[0] + self.item_padding[0] - self.position[0]
-            if value > self.get_size()[0]:
-                self._items_pos = self.position[0] + self.padding[0], \
-                                  self._items_pos[1] + i.get_size()[1] + self.item_padding[1]
-            i.set_position(self._items_pos)
-            self._items_pos = self._items_pos[0] + i.get_size()[0] + self.item_padding[0], self._items_pos[1]
-
         self._items_pos = self.position[0] + self.padding[0], self.position[1] + self.padding[1]
+        for i in self._data:
+            self._set_item_position(i)
+            self._items_pos = i.position[0] + i.get_size()[0] + self.item_padding[0], self._items_pos[1]
+
+    def _set_item_position(self, item: ListItem):
+        value = item.get_size()[0] + self._items_pos[0] + self.item_padding[0] - self.position[0]
+        if value > self.get_size()[0]:
+            self._items_pos = self.position[0] + self.padding[0], \
+                              self._items_pos[1] + item.get_size()[1] + self.item_padding[1]
+        item.set_position(self._items_pos)
 
     def add_item(self, item: ListItem) -> None:
+        if len(self._data) > 0:
+            last = self._data[-1]
+            self._items_pos = last.position[0] + last.get_size()[0] + self.item_padding[0], self._items_pos[1]
         self._data.append(item)
-        self._redraw_list()
+        self._set_item_position(item)
 
     def remove_item(self, item: ListItem) -> None:
         self._data.remove(item)
