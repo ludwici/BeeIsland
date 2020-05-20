0beda24560f859ce69b63b2b26142e60918bdc23
Andrew <gammaclone@gmail.com>
2020-05-21 00:12:17 +0300

    Simple encapsulation level up

--- src/Scenes/Map/MapZone.py
+++ src/Scenes/Map/MapZone.py
@@ -14,26 +14,26 @@ class MapZone:
         self.parent = parent
         self.border_image = pygame.image.load("../res/images/zones/border_{0}.png".format(self.name)).convert_alpha()
         self.__is_lock = True
-        self.zone_rect = self.border_image.get_rect()
-        self.zone_rect.x = pos_x
-        self.zone_rect.y = pos_y
+        self.__zone_rect = self.border_image.get_rect()
+        self.__zone_rect.x = pos_x
+        self.__zone_rect.y = pos_y
         self.quest_list = []
         self.quest_icons = []
 
-        self.click_rect = copy.copy(self.zone_rect)
+        self.click_rect = copy.copy(self.__zone_rect)
         self.click_rect.height -= 30
         self.click_rect.width -= 50
         self.click_rect.x += 35
 
-        self.has_fog = has_fog
+        self.__has_fog = has_fog
         self.show_border = False
-        self.fog_rect = None
+        self.__fog_rect = None
         try:
             self.fog_image = pygame.image.load("../res/images/zones/fog_{0}.png".format(self.name)).convert_alpha()
-            self.fog_rect = self.fog_image.get_rect()
-            self.fog_rect.center = self.zone_rect.center
+            self.__fog_rect = self.fog_image.get_rect()
+            self.__fog_rect.center = self.__zone_rect.center
         except pygame.error:
-            self.has_fog = False
+            self.__has_fog = False
 
     @property
     def is_lock(self) -> bool:
@@ -51,7 +51,7 @@ class MapZone:
 
     def unlock(self) -> None:
         self.__is_lock = False
-        self.has_fog = False
+        self.__has_fog = False
 
     def on_mouse_over(self) -> None:
         self.show_border = True
@@ -73,7 +73,7 @@ class MapZone:
 
     def draw(self, screen: pygame.Surface) -> None:
         if self.show_border:
-            screen.blit(self.border_image, self.zone_rect)
+            screen.blit(self.border_image, self.__zone_rect)
         [qi.draw(screen) for qi in self.quest_icons]
-        if self.has_fog:
-            screen.blit(self.fog_image, self.fog_rect)
+        if self.__has_fog:
+            screen.blit(self.fog_image, self.__fog_rect)
