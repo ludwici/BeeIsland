dfe251f35a06ed0511c8de8304cafd8d9291bc10
Andrew <gammaclone@gmail.com>
2020-05-20 20:35:11 +0300

    Added read data from json file Localization field now be static

--- src/Database/Localization.py
+++ src/Database/Localization.py
@@ -1,3 +1,4 @@
+import json
 from enum import Enum
 
 
@@ -8,9 +9,29 @@ class LocalList(Enum):
 
 
 class Localization:
-    def __init__(self, locale: LocalList) -> None:
-        self.__current_locale = locale
+    __current_locale = LocalList.RU
 
-    @property
-    def current_locale(self):
-        return self.__current_locale.value[0]
+    def __init__(self, scene_name: str) -> None:
+        self.__scene_name = scene_name
+        self.__data = self.__read_locale()
+
+    def __read_locale(self):
+        locale_path = "../res/locales/scenes/{0}.json".format(self.__scene_name,
+                                                              Localization.get_current_locale())
+        with open(locale_path, encoding="utf-8") as json_file:
+            data = json.load(json_file)
+        return data
+
+    def get_string(self, str_id: str) -> str:
+        return self.get_params_by_string(str_id)["text"]
+
+    def get_params_by_string(self, str_id: str):
+        return self.__data[Localization.get_current_locale()][str_id]
+
+    @staticmethod
+    def set_locale(locale: LocalList):
+        Localization.__current_locale = locale
+
+    @staticmethod
+    def get_current_locale():
+        return Localization.__current_locale.value[0]
