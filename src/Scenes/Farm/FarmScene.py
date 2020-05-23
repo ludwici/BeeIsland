7c0a19d7a58d10e5ec77f8173f89263f4786c415
Andrew <gammaclone@gmail.com>
2020-05-23 07:07:04 +0300

    Bee nests are radio buttons now

--- src/Scenes/Farm/FarmScene.py
+++ src/Scenes/Farm/FarmScene.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.event import Event
 
 from Scenes.Farm.ModifyMenu import ModifyMenu
+from UI.RadioGroup import RadioGroup
 from src import Constants
 from src.Scenes.Scene import Scene
 from src.UI.BeeNestButton import BeeNestButton
@@ -15,7 +16,6 @@ class FarmScene(Scene):
         self.bg_image = pygame.image.load("../res/images/farm1_bg.jpg").convert()
         self.main_image_rect = self.main_image.get_rect()
         self.main_image_rect.center = (Constants.WINDOW_W / 2, Constants.WINDOW_H / 2)
-        self.bee_nest_list = []
         self.to_map_button = Button(parent=self, normal_image_path="../res/images/buttons/to_map_normal.png",
                                     position=(0, 0))
         self.to_map_button.set_image_by_state(ButtonState.HOVERED, "../res/images/buttons/to_map_hover.png")
@@ -29,12 +29,12 @@ class FarmScene(Scene):
         bg_x = self.main_image_rect.x
         bg_y = self.main_image_rect.y
         positions = [(194, 104), (501, 104), (111, 340), (584, 340), (194, 577), (501, 577)]
+        self.nest_group = RadioGroup()
         for i in range(6):
             bee_nest = BeeNestButton(parent=self, normal_image_path="../res/images/bee/hive/hive1_empty_normal.png",
-                                     state=ButtonState.NORMAL,
+                                     state=ButtonState.NORMAL, group=self.nest_group,
                                      position=(bg_x + positions[i][0], bg_y + positions[i][1]))
             bee_nest.set_image_by_state(ButtonState.HOVERED, "../res/images/bee/hive/hive1_empty_hover.png")
-            self.bee_nest_list.append(bee_nest)
 
     def show_modify(self):
         m = ModifyMenu(parent=self)
@@ -44,7 +44,7 @@ class FarmScene(Scene):
         [d.update(dt) for d in self._drawable_list]
 
     def handle_events(self, event: Event) -> None:
-        [bn.handle_event(event) for bn in self.bee_nest_list]
+        self.nest_group.handle_event(event)
         [d.handle_event(event) for d in self._drawable_list]
         self.to_map_button.handle_event(event)
         self.to_upgrade_button.handle_event(event)
@@ -53,7 +53,7 @@ class FarmScene(Scene):
         surface.fill((0, 0, 0))
         surface.blit(self.bg_image, self.bg_image.get_rect())
         surface.blit(self.main_image, self.main_image_rect)
-        [bn.draw(surface) for bn in self.bee_nest_list]
+        self.nest_group.draw(surface)
         [d.draw(surface) for d in self._drawable_list]
         self.to_map_button.draw(surface)
         self.to_upgrade_button.draw(surface)
