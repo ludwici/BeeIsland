63bd5eb21b5151316409d8d77cc420d61563ce44
Andrew <gammaclone@gmail.com>
2020-05-27 04:34:10 +0300

    Added bonuses to bee

--- src/BeeFamily/BeeWorker.py
+++ src/BeeFamily/BeeWorker.py
@@ -1,7 +1,22 @@
+from BeeFamily.Bonuses.IBonus import IBonus
+from Quests.Questable import Questable
 from src.BeeFamily.Bee import Bee
 
 
 class BeeWorker(Bee):
-    def __init__(self, parent, position: (int, int) = (0, 0), level: int = 1) -> None:
+    def __init__(self, parent, position: (int, int) = (0, 0), level: int = 1, bonus: IBonus = None) -> None:
         Bee.__init__(self, parent=parent, position=position, level=level)
         self.set_image("../res/images/bee/bee1.png")
+        self._can_hard = False
+        self._bonus = bonus
+
+    @property
+    def can_hard(self) -> bool:
+        return self._can_hard
+
+    @property
+    def bonus(self) -> str:
+        return str(self._bonus)
+
+    def setup_bonus(self, q: Questable):
+        self._bonus.setup_bonus(q)
---
+++ src/BeeFamily/Bonuses/IBonus.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+
+from Quests.Questable import Questable
+
+
+class IBonus(ABC):
+    def __init__(self) -> None:
+        pass
+
+    @abstractmethod
+    def setup_bonus(self, q: Questable) -> None:
+        pass
+
+    @abstractmethod
+    def __str__(self) -> str:
+        pass
+
+
+class TimeBonus(IBonus):
+    def __init__(self, time_val) -> None:
+        IBonus.__init__(self)
+        self.__time = time_val
+
+    def setup_bonus(self, q: Questable) -> None:
+        q.time += self.__time
+
+    def __str__(self) -> str:
+        return "+{0} к времени".format(self.__time)
+
+
+class ScoreBonus(IBonus):
+    def __init__(self, score_val) -> None:
+        IBonus.__init__(self)
+        self.__score = score_val
+
+    def setup_bonus(self, q: Questable) -> None:
+        q.score_modifier_percent += self.__score
+
+    def __str__(self) -> str:
+        return "+{0}% к общему счёту".format(self.__score)
