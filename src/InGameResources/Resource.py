f3df8306c6ec8d1808fadef190d9c04081143ad2
Andrew <gammaclone@gmail.com>
2020-05-28 06:26:20 +0300

    Added check for base_value Added override operator -

--- src/InGameResources/Resource.py
+++ src/InGameResources/Resource.py
@@ -2,38 +2,55 @@ from copy import copy
 
 
 class Resource:
-    __slots__ = ("locale_name", "value", "_base_value", "max_value")
+    __slots__ = ("locale_name", "__value", "__base_value", "max_value")
 
     def __init__(self, locale_name, amount=0, max_value=0) -> None:
         self.locale_name = locale_name
-        self.value = amount
-        self._base_value = amount
+        self.__value = amount
+        self.__base_value = amount
         self.max_value = max_value
 
     def increaseByPercent(self, percent) -> None:
-        self.value = self.base_value
-        self.value += self.base_value * percent / 100
+        self.__value = self.base_value
+        self.__value += self.base_value * percent / 100
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, v: int):
+        self.__value = v
+        self.base_value = v
 
     @property
     def base_value(self) -> int:
-        return copy(self._base_value)
+        return self.__base_value
 
     @base_value.setter
     def base_value(self, value) -> None:
-        self._base_value = value
+        self.__base_value = value
+        if self.__base_value < 0:
+            self.__base_value = 0
+        if self.__base_value > self.max_value:
+            self.__base_value = self.max_value
 
     def __add__(self, other) -> "Resource":
         if self.locale_name == other.locale_name:
-            self.value += other.value
+            self.base_value += other.base_value
         return self
 
+    def __sub__(self, other) -> "Resource":
+        if self.locale_name == other.locale_name:
+            self.base_value -= other.base_value
+
     def __bool__(self) -> bool:
-        return self.value > 0
+        return self.__value > 0
 
     def __str__(self) -> str:
-        return str(self.value)
+        return str(self.__value)
 
     def __repr__(self) -> str:
-        r = "<Resource(name={0}, value={1}, base={2}, max_count={3})>".format(self.locale_name, self.value,
+        r = "<Resource(name={0}, value={1}, base={2}, max_count={3})>".format(self.locale_name, self.__value,
                                                                               self.base_value, self.max_value)
         return r
