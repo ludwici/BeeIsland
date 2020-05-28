edb4a31946bddf9fb9ec36a136bfab8c37707b69
Andrew <gammaclone@gmail.com>
2020-05-28 12:11:13 +0300

    Added method for find resources

--- src/Utils.py
+++ src/Utils.py
@@ -1,5 +1,13 @@
+import os
+import sys
 from math import hypot
 
 
 def get_distance(source, dest) -> float:
     return hypot(dest[0] - source[0], dest[1] - source[1])
+
+
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath(".."), relative_path).replace("\\", '/')
