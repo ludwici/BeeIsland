a674d3b62f38db62732624c0923ceaace68f910f
Andrew <gammaclone@gmail.com>
2020-05-28 12:12:20 +0300

    Added support for resources

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import os
+import sys
+
 from src import Constants
 from src.RenderWindow import RenderWindow
 
 
 def main():
+    if getattr(sys, 'frozen', False):
+        os.chdir(sys._MEIPASS)
+
     main_window = RenderWindow(width=Constants.WINDOW_W, height=Constants.WINDOW_H)
     main_window.start()
 
