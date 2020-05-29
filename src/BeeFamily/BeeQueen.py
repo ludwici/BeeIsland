ad8164b1cd5e42c759911c9ace68d5e10e670620
Andrew <gammaclone@gmail.com>
2020-05-29 09:28:06 +0300

    Only female, now

--- src/BeeFamily/BeeQueen.py
+++ src/BeeFamily/BeeQueen.py
@@ -3,5 +3,5 @@ from src.BeeFamily.Bee import Bee
 
 class BeeQueen(Bee):
     def __init__(self, parent, position: (int, int) = (0, 0), level: int = 1) -> None:
-        Bee.__init__(self, parent=parent, position=position, level=level, bonus=None)
+        Bee.__init__(self, parent=parent, position=position, level=level, bonus=None, sex=Bee.BeeSex.FEMALE)
         self.set_image("/bee/queen1.png")
