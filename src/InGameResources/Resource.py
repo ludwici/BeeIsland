c3a025d247c7794f1ac2ece2fbb84a170c715009
Andrew <gammaclone@gmail.com>
2020-05-30 16:49:40 +0300

    Added description Added type hints Added missed return expression

--- src/InGameResources/Resource.py
+++ src/InGameResources/Resource.py
@@ -1,8 +1,9 @@
 class Resource:
-    __slots__ = ("locale_name", "__value", "__base_value", "max_value")
+    __slots__ = ("locale_name", "__value", "__base_value", "max_value", "locale_desc")
 
-    def __init__(self, locale_name, amount=0, max_value=0) -> None:
+    def __init__(self, locale_name, locale_desc="", amount=0, max_value=0) -> None:
         self.locale_name = locale_name
+        self.locale_desc = locale_desc
         self.__value = amount
         self.__base_value = amount
         self.max_value = max_value
@@ -12,11 +13,11 @@ class Resource:
         self.__value += self.base_value * percent / 100
 
     @property
-    def value(self):
-        return self.__value
+    def value(self) -> int:
+        return int(self.__value)
 
     @value.setter
-    def value(self, v: int):
+    def value(self, v: int) -> None:
         self.__value = v
         self.base_value = v
 
@@ -40,6 +41,7 @@ class Resource:
     def __sub__(self, other) -> "Resource":
         if self.locale_name == other.locale_name:
             self.base_value -= other.base_value
+        return self
 
     def __bool__(self) -> bool:
         return self.__value > 0
