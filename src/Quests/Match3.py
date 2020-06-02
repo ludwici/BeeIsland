8d60dc41f49c90d2f43d6217eb3eebc329c962de
Andrew <gammaclone@gmail.com>
2020-06-02 20:29:15 +0300

    Removed rudimentary class

--- src/Quests/Match3.py
+++
@@ -1,7 +0,0 @@
-from src.Quests.QuestTemplate import QuestTemplate
-from src.Quests.Questable import Questable
-
-
-class Match3(Questable):
-    def __init__(self, quest_template: QuestTemplate, icon_offset: (int, int)) -> None:
-        Questable.__init__(self, quest_template=quest_template, icon_offset=icon_offset)
