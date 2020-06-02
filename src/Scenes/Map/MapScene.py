d9d3680ee6308735159aaadbf8424005e686800e
Andrew <gammaclone@gmail.com>
2020-06-02 13:16:43 +0300

    Added autoclose menu

--- src/Scenes/Map/MapScene.py
+++ src/Scenes/Map/MapScene.py
@@ -32,6 +32,7 @@ class MapScene(Scene):
         self.init_zones()
 
     def show_quest_menu(self, quest) -> None:
+        self.remove_drawable(self.find_drawable_by_type(QuestMenu))
         q = QuestMenu(parent=self, quest=quest)
         self.add_drawable(q)
 
