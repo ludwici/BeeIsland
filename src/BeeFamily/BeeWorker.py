6d87f4bbf9052bb0281e773981bce6785d6f5ad5
Andrew <gammaclone@gmail.com>
2020-06-05 06:45:59 +0300

    Create standard bonus pool

--- src/BeeFamily/BeeWorker.py
+++ src/BeeFamily/BeeWorker.py
@@ -1,8 +1,10 @@
 from src.BeeFamily.Bee import Bee
-from src.BeeFamily.Bonuses.IBonus import IBonus
+from src.BeeFamily.Bonuses.IBonus import IBonus, ScoreBonus, TimeBonus
 
 
 class BeeWorker(Bee):
     def __init__(self, parent, position: (int, int) = (0, 0), level: int = 1, bonus: IBonus = None) -> None:
+        if not bonus:
+            bonus = IBonus.get_random_bonus([TimeBonus(), ScoreBonus()])
         Bee.__init__(self, parent=parent, position=position, level=level, bonus=bonus)
         self.set_image("/bee/bee1.png")
