05348d2755723a073a572cc72d497a017caae15c
Andrew <gammaclone@gmail.com>
2020-06-05 20:10:22 +0300

    Added completed quests and unlocked zones

--- src/Scenes/Map/MapScene.py
+++ src/Scenes/Map/MapScene.py
@@ -12,7 +12,8 @@ from src.UI.PopupNotify import PopupNotify
 
 
 class MapScene(Scene):
-    __slots__ = ("__map_image", "__bg_map_rect", "zones", "to_farm_button", "__bg_image", "__bg_rect")
+    __slots__ = ("__map_image", "__bg_map_rect", "zones", "to_farm_button", "__bg_image", "__bg_rect",
+                 "__unlocked_zones", "__completed_quests")
 
     def __init__(self, main_window, name, player) -> None:
         Scene.__init__(self, main_window=main_window, player=player, name=name)
@@ -21,6 +22,10 @@ class MapScene(Scene):
         self.__map_image = pygame.image.load("{0}/images/map1.jpg".format(self._res_dir)).convert()
         self.__bg_map_rect = self.__map_image.get_rect()
         self.zones = []
+        self.__unlocked_zones = set()
+        self.__unlocked_zones.add(1)
+        self.__completed_quests = set()
+        self.__completed_quests.add(1)
         self.to_farm_button = Button(parent=self, position=(10, 10), normal_image_path="to_farm_normal.png")
         self.to_farm_button.set_image_by_state(ButtonState.HOVERED, "to_farm_hover.png")
         self.to_farm_button.add_action({ButtonEventType.ON_CLICK_LB: lambda: self.change_scene("Farm")})
@@ -36,26 +41,35 @@ class MapScene(Scene):
 
     def init_zones(self) -> None:
         bg_x, bg_y = self.__bg_map_rect.x, self.__bg_map_rect.y
-        zone1 = MapZone(self, "zone1", position=(bg_x + 20, bg_y + 75))
-        zone2 = MapZone(self, "zone2", position=(bg_x + 83, bg_y + 375))
-        zone3 = MapZone(self, "zone3", position=(bg_x + 488, bg_y + 183))
-        zone4 = MapZone(self, "zone4", position=(bg_x + 298, bg_y + 33))
-        zone5 = MapZone(self, "zone5", position=(bg_x + 415, bg_y + 465))
-        zone6 = MapZone(self, "zone6", position=(bg_x + 285, bg_y + 214))
-        zone7 = MapZone(self, "zone7", position=(bg_x + 383, bg_y + 578))
-        zone1.unlock()
+        positions = [(bg_x + 20, bg_y + 75), (bg_x + 83, bg_y + 375), (bg_x + 488, bg_y + 183), (bg_x + 298, bg_y + 33),
+                     (bg_x + 415, bg_y + 465), (bg_x + 285, bg_y + 214), (bg_x + 383, bg_y + 578)]
+
+        for i in range(7):
+            zone = MapZone(self, i + 1, position=(positions[i][0], positions[i][1]))
+            self.zones.append(zone)
 
         db = Database.get_instance()
         quest_data = db.get_all_quests()
+
         for qd in quest_data:
             quest = Quest(quest_template=qd, icon_offset=(bg_x, bg_y))
-            zone1.add_quest(quest)
+            available = quest.quest_id in self.__completed_quests
+            self.zones[quest.zone - 1].add_quest(quest, available)
 
-        self.zones.extend([zone1, zone2, zone3, zone4, zone5, zone6, zone7])
+        [z.lock() for z in self.zones]
+        for i in self.__unlocked_zones:
+            self.zones[i - 1].unlock()
 
     def on_scene_change(self) -> None:
         super().on_scene_change()
         self.zones.clear()
+
+    def complete_quest(self, quest_id: int):
+        self.__completed_quests.add(quest_id + 1)
+        if quest_id == 4:
+            self.__unlocked_zones.add(2)
+        elif quest_id == 6:
+            self.__unlocked_zones.add(3)
 
     def handle_events(self, event: Event) -> None:
         if event.type == pygame.KEYDOWN:
