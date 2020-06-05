712cdb688e5f44cb1c0fdb718934a94f9176befd
Andrew <gammaclone@gmail.com>
2020-06-05 06:45:41 +0300

    Added bonuses to standard pool Change image

--- src/BeeFamily/BeeWarrior.py
+++ src/BeeFamily/BeeWarrior.py
@@ -1,8 +1,11 @@
 from src.BeeFamily.Bee import Bee
-from src.BeeFamily.Bonuses.IBonus import IBonus
+from src.BeeFamily.Bonuses.IBonus import IBonus, RandomResourceBonus, IncreaseResourcesBonus, SpeedUpBonus, HealthBonus
 
 
 class BeeWarrior(Bee):
     def __init__(self, parent, position: (int, int) = (0, 0), level: int = 1, bonus: IBonus = None) -> None:
+        if not bonus:
+            bonus = IBonus.get_random_bonus([RandomResourceBonus(), IncreaseResourcesBonus(),
+                                             SpeedUpBonus(), HealthBonus()])
         Bee.__init__(self, parent=parent, position=position, level=level, bonus=bonus)
-        self.set_image("/bee/bee1.png")
+        self.set_image("/bee/bee2.png")
