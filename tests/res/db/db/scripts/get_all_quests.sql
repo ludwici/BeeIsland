ef5ae311449c98fb24f783e6f25b9afadfdbdf55
ludwici <andrew.volski@gmail.com>
2020-06-07 16:32:19 +0300

    Update resources

---
+++ tests/res/db/db/scripts/get_all_quests.sql
@@ -0,0 +1,18 @@
+select q.id,
+       case ?
+           when 'ua' then ql.title_ua
+           when 'en' then ql.title_en
+           else ql.title_ru
+           end as title,
+       case ?
+           when 'ua' then ql.desc_ua
+           when 'en' then ql.desc_en
+           else ql.desc_ru
+           end as description,
+       q.icon_x,
+       q.icon_y,
+       q.type,
+       q.zone
+from quest q,
+     quest_locale ql
+where q.type = ql.id;
