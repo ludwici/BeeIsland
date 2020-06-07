87f0361f82dc714a49af439a64e5e8931c7ca9fe
Andrew <gammaclone@gmail.com>
2020-06-07 13:24:19 +0300

    Set code before ctors

--- src/DNAEntity.py
+++ src/DNAEntity.py
@@ -6,17 +6,17 @@ from src.Interfaces.RenderObject import RenderObject
 
 class DNAEntity(RenderObject, GeneCode):
     def __init__(self, parent, dna_type: str, r) -> None:
+        if dna_type == "worker":
+            code = "1"
+        elif dna_type == "warrior":
+            code = "2"
+        else:
+            code = "3"
         RenderObject.__init__(self, parent=parent)
-        GeneCode.__init__(self)
+        GeneCode.__init__(self, code=code)
         self.__image = pygame.image.load("{0}/dna_{1}.png".format(self._res_dir, dna_type)).convert_alpha()
         self.__dna_type = dna_type
         self.resource = r
-        if dna_type == "worker":
-            self.dna_code = "1"
-        elif dna_type == "warrior":
-            self.dna_code = "2"
-        else:
-            self.dna_code = "3"
         self._rect = self.__image.get_rect()
 
     def draw(self, screen: pygame.Surface) -> None:
