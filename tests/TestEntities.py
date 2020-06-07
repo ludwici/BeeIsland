32b11a657429d0541463b770af1381eda4dbe42e
ludwici <andrew.volski@gmail.com>
2020-06-07 16:31:53 +0300

    Updated to new modify system

--- tests/TestEntities.py
+++ tests/TestEntities.py
@@ -1,6 +1,10 @@
 import sys
 import unittest
 
+from src.BeeFamily.BeeQueen import BeeQueen
+from src.BeeFamily.BeeWarrior import BeeWarrior
+from src.BeeFamily.BeeWorker import BeeWorker
+
 sys.path.append('../')
 from src.BeeNest import BeeNest
 from src.BeeFamily.Bee import Bee
@@ -8,7 +12,7 @@ from src.BeeFamily.Bee import Bee
 
 class TestEntities(unittest.TestCase):
     def setUp(self) -> None:
-        self.bee = Bee(parent=None, bonus=None)
+        self.bee = Bee(parent=None, bonus=None, code="A")
         self.nest = BeeNest()
 
     def test_bee_xp_1(self):
@@ -26,11 +30,42 @@ class TestEntities(unittest.TestCase):
         self.check_bee_min_max()
 
     def test_add_bee_to_nest(self):
-        b = Bee(parent=None, bonus=None)
+        b = Bee(parent=None, bonus=None, code="A")
 
         for i in range(self.nest.max_size):
             if not self.nest.add_bee(b):
                 self.fail("Too many bees")
+
+    def test_modify1(self):
+        b1 = self.modify("A", "A")
+        if not isinstance(b1, BeeWorker) and not isinstance(b1, BeeWarrior):
+            self.fail("Unresolved type")
+        b2 = self.modify("B", "B")
+        if not isinstance(b2, BeeWorker) and not isinstance(b2, BeeWarrior):
+            self.fail("Unresolved type")
+        b3 = self.modify("A", "B")
+        if not isinstance(b3, BeeWorker) and not isinstance(b3, BeeWarrior):
+            self.fail("Unresolved type")
+        b4 = self.modify("1", "1")
+        if not isinstance(b4, BeeWorker) and not isinstance(b4, BeeWarrior):
+            self.fail("Unresolved type")
+
+        self.assertIsInstance(self.modify("A", "1"), BeeWorker)
+        self.assertIsInstance(self.modify("A", "J"), Bee)
+
+        self.assertIsInstance(self.modify("B", "2"), BeeWarrior)
+        self.assertIsInstance(self.modify("B", "J"), Bee)
+        self.assertIsInstance(self.modify("2", "2"), BeeWarrior)
+
+        self.assertIsInstance(self.modify("3", "3"), BeeQueen)
+
+        self.assertIsNone(self.modify("Q", "Q"))
+        self.assertIsNone(self.modify("Q", "J"))
+
+    def modify(self, code1, code2):
+        b1 = Bee(parent=None, bonus=None, code=code1)
+        b2 = Bee(parent=None, bonus=None, code=code2)
+        return b1 + b2
 
     def check_bee_min_max(self):
         self.assertGreaterEqual(self.bee.current_xp, 1)
