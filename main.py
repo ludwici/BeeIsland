35ac843a7042bfad94783bbb3e13de8897db8461
Andrew <gammaclone@gmail.com>
2020-06-12 14:26:06 +0300

    Update main

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ def main():
     if getattr(sys, 'frozen', False):
         os.chdir(sys._MEIPASS)
 
-    try:
-        print(platform.architecture()[0], sys.maxsize > 2 ** 32)
-        main_window = RenderWindow(width=Constants.WINDOW_W, height=Constants.WINDOW_H)
-        main_window.start()
-    except Exception as ex:
-        print(ex)
+    # try:
+    print(platform.architecture()[0], sys.maxsize > 2 ** 32)
+    main_window = RenderWindow(width=Constants.WINDOW_W, height=Constants.WINDOW_H)
+    main_window.start()
+    # except Exception as ex:
+    #     print(ex)
 
 
 if __name__ == "__main__":
